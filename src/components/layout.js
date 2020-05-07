9a25956a414437071e2ff959aaa5202393c0df73
thomasgui <guilhem.thomas@gmail.com>
2020-05-07 21:42:59 +0200

    about contenfull variable

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,11 +12,13 @@ import "./bootstrap.min.css";
 
 import "./layout.css";
 import Navbar from "./Globals/Navbar";
+import Footer from "./Globals/Footer";
 
 const Layout = ({ children }) => (
   <>
     <Navbar />
     {children}
+    <Footer />
   </>
 );
 Layout.propTypes = {
